370c9bbec85f00ecccbec1e13bf369a37b8b8f2e
Sam Collins <theatrepro11@gmail.com>
2018-11-14 17:42:16 +0000

    Updated to work with JWT endpoint

--- door-entry-system.py
+++ door-entry-system.py
@@ -1,78 +1,43 @@
-import RPi.GPIO as gpio
-import time
 import datetime
 import jwt
 import requests
 import nfc
+import os
 import logging
+
 from dotenv import load_dotenv
 load_dotenv()
 
-LED_PIN = 17
-DOOR_LATCH_PIN = 18
-
-gpio.setmode(gpio.BCM)
-gpio.setwarnings(False)
-gpio.setup(LED_PIN, gpio.OUT)
-gpio.setup(DOOR_LATCH_PIN, gpio.OUT)
+logging.basicConfig(filename='/tmp/rfid.log', level=logging.DEBUG)
 
 
-DEVICE_ID = '62ef7d94-4c56-41ba-8694-550e4b0ef5e9'
-SECRET = 'kjlsdlkdskldf'
-logging.basicConfig(
-    filename='/tmp/rfid.log',
-    format='%(levelname)s:%(message)s',
-    level=logging.DEBUG)
-
-
-def trigger_door(mode=False):
-    if mode is True:
-        print('door activate')
-        gpio.output(DOOR_LATCH_PIN, gpio.HIGH)
-    time.sleep(1)
-    gpio.output(DOOR_LATCH_PIN, gpio.LOW)
-
-
-def flash_led(mode=False):
-    if mode is True:
-        gpio.output(LED_PIN, gpio.HIGH)
-        time.sleep(1)
-        gpio.output(LED_PIN, gpio.LOW)
-    if mode is False:
-        for _ in range(0, 3):
-            gpio.output(LED_PIN, gpio.HIGH)
-            time.sleep(0.1)
-            gpio.output(LED_PIN, gpio.LOW)
-            time.sleep(0.1)
-    time.sleep(1)
-
-
-def open():
-    trigger_door(True)
-    flash_led(True)
+def allow():
     logging.info('Request allowed')
+    pass
 
 
 def deny():
-    trigger_door(False)
-    flash_led()
     logging.info('Request denied')
+    pass
 
 
 def check_valid(rfid_token):
     encoded = jwt.encode({
         'rfid_code': rfid_token,
-        'device_id': DEVICE_ID,
+        'device_id': os.getenv('DEVICE_ID'),
         'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=2)
-    }, SECRET, algorithm='HS256')
+    }, os.getenv('SECRET'), algorithm='HS256')
+    logging.debug("Sending request with data: %s" % encoded)
+    response = requests.post(os.getenv('URL', 'https://maidstone-hackspace.org.uk/api/v1/rfid_auth/'), data={"data":encoded}, timeout=3)
+    if response.status_code != 200:
+        logging.info("Response code not valid")
+        return deny()
+    logging.debug("Response body: %s" % response.json())
+    parsed_response = jwt.decode(response.json(), os.getenv('SECRET'), algorithms=['HS256'], verify=False)
 
-    # response = requests.post('https://maidstone-hackspace.org.uk/rfid', data=encoded)
-    body = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoZW50aWNhdGVkIjp0cnVlLCJleHAiOjE1MTgwMzA0ODR9.gmarIZPt8HML1WzHqmlau5E9o0FyrMuS_pxlo3vfOuw'
-
-    thing = jwt.decode(body, SECRET, algorithms=['HS256'])
-
-    if thing['authenticated'] is True:
-        open()
+    if parsed_response['authenticated'] is True:
+        logging.info("User %s has been authenticated" % parsed_response['username'])
+        allow()
     else:
         deny()
 
@@ -86,8 +51,6 @@ while True:
         logging.info('Detected RFID card %s' % code)
         try:
             check_valid(code)
-            flash_led(True)
         except Exception as e:
             logging.exception(e)
-            flash_led(False)
             deny()
